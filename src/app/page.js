aef32a0b36767fc7ce7058fd3495a9b9effaaccc
Kei07r <kei.skillsboost@gmail.com>
2025-10-22 17:15:13 +0530

    Feat: Updated Header, Added favicon

--- src/app/page.js
+++ src/app/page.js
@@ -4,21 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="min-w-full min-h-full flex flex-col items-center justify-center gap-4">
-        <div className="bg-card flex flex-col gap-4 items-center border-2 p-10 rounded-lg shadow-lg">
-          <p className="text-lg font-medium"> Hello, may I have your name? </p>
-          <form className="flex flex-col gap-4 items-center">
-            <input
-              type="text"
-              placeholder="Your name"
-              className="border border-border rounded-md p-3 w-64 focus:outline-none focus:ring-2 focus:ring-primary"
-            />
-            <Link href="/home">
-              <Button className="min-w-full max-w-[300px] shadow-lg">Sign-In</Button>
-            </Link>
-          </form>
-        </div>
-      </div>
+    
     </>
   );
 }
--- src/customComponents/Header.jsx
+++ src/customComponents/Header.jsx
@@ -1,9 +1,20 @@
+import logo from "../../public/logo.png";
+import Image from "next/image";
+
 const Header = () => {
   return (
-    <header className="min-w-full min-h-[100px] bg-primary flex items-center justify-center mb-8">
-      <h1 className="text-3xl font-bold text-primary-foreground">
-        Agentic To-Do Simulator
-      </h1>
+    <header>
+      <div className="w-full flex items-center justify-center mx-auto px-4 py-6">
+        <Image
+          src={logo}
+          alt="Agentic Todo Sim Logo"
+          width={50}
+          height={50}
+          className="mr-4 rounded-4xl"/>
+        <h1 className="text-3xl font-bold text-center">
+          Agentic Todo Sim
+        </h1>
+      </div>
     </header>
   );
 };
