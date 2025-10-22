572d670fbd9929aeefaa608ae42c4c688bc86d81
Kei07r <kei.skillsboost@gmail.com>
2025-10-22 20:20:08 +0530

    Feat: Add Tabs component with Radix UI integration and implement Chat page with tabbed interface

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -1,9 +1,38 @@
-const Home = () => {
-    return (
-        <>
-        <h1>Hello, World!</h1>
-        </>
-    )
-}
+"use client";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-export default Home;
+const Chat = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <Tabs defaultValue="To-Do's" className="items-center w-[400px]">
+          <TabsList>
+            <TabsTrigger value="To-Do's">To-Do's</TabsTrigger>
+            <TabsTrigger value="Chat">Chat</TabsTrigger>
+            <TabsTrigger value="Logs">Logs</TabsTrigger>
+          </TabsList>
+          <TabsContent value="To-Do's">
+            Make changes to your To-Do's here.
+          </TabsContent>
+          <TabsContent value="Chat">
+            Chat here
+          </TabsContent>
+          <TabsContent value="Logs">
+            View your logs here.
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
+};
+
+export default Chat;
